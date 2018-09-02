3563c31ef71946ad0907c0229a01e29733f071d2
Franklin van de Meent <fr@nkl.in>
2018-09-02 07:32:17 +0200

    Style(test): Reduced indentation whitespace

--- test.js
+++ test.js
@@ -19,66 +19,57 @@ const app = pkg && pkg (config);
 
 dotest.add ('Interface', test => {
   test()
-    .isFunction ('fail', 'pkg', pkg)
-    .isFunction ('fail', 'app', app)
-    .done()
-  ;
+  .isFunction ('fail', 'pkg', pkg)
+  .isFunction ('fail', 'app', app)
+  .done();
 });
 
 
 dotest.add ('Communication', test => {
   app (params)
-    .then (test.info)
-    .then (data => {
-      test()
-        .isObject ('fail', 'data', data)
-        .isUndefined ('fail', 'data.error', data && data.error)
-        .isArray ('fail', 'data.extremes', data && data.extremes)
-        .isNotEmpty ('fail', 'data.extremes', data && data.extremes)
-        .done()
-      ;
-    })
-    .catch (test)
-  ;
+  .then (test.info)
+  .then (data => {
+    test()
+    .isObject ('fail', 'data', data)
+    .isUndefined ('fail', 'data.error', data && data.error)
+    .isArray ('fail', 'data.extremes', data && data.extremes)
+    .isNotEmpty ('fail', 'data.extremes', data && data.extremes)
+    .done();
+  })
+  .catch (test);
 });
 
 
 dotest.add ('Error handling: API error', test => {
   app ({})
-    .then (data => {
-      test()
-        .isUndefined ('fail', 'data', data)
-        .done()
-      ;
-    })
-    .catch (err => {
-      test()
-        .isError ('fail', 'err', err)
-        .isNotEmpty ('fail', 'err.message', err && err.message)
-        .isNumber ('fail', 'err.statusCode', err && err.statusCode)
-        .done()
-      ;
-    })
-  ;
+  .then (data => {
+    test()
+    .isUndefined ('fail', 'data', data)
+    .done();
+  })
+  .catch (err => {
+    test()
+    .isError ('fail', 'err', err)
+    .isNotEmpty ('fail', 'err.message', err && err.message)
+    .isNumber ('fail', 'err.statusCode', err && err.statusCode)
+    .done();
+  });
 });
 
 
 dotest.add ('Error handling: Client error', test => {
   app ()
-    .then (data => {
-      test()
-        .isUndefined ('fail', 'data', data)
-        .done()
-      ;
-    })
-    .catch (err => {
-      test()
-        .isError ('fail', 'err', err)
-        .isNotEmpty ('fail', 'err.message', err && err.message)
-        .done()
-      ;
-    })
-  ;
+  .then (data => {
+    test()
+    .isUndefined ('fail', 'data', data)
+    .done();
+  })
+  .catch (err => {
+    test()
+    .isError ('fail', 'err', err)
+    .isNotEmpty ('fail', 'err.message', err && err.message)
+    .done();
+  });
 });
 
 

fdeea9d37b772a5e15f23532df2bd8c762a9e69e
Franklin <fr@nkl.in>
2021-10-05 23:50:42 +0200

    Feat: Auto convert input dates
    It translates date, start and localtime to the right format

--- index.js
+++ index.js
@@ -11,6 +11,44 @@ const { doRequest } = require ('httpreq');
 
 
 /**
+ * Convert a date to a string
+ *
+ * @param   {Date|string}  [date]  Date to convert
+ * @param   {string}       type    Return a long ISO string
+ *
+ * @return  {string}
+ */
+
+async function fixDate (date, type) {
+  if (!date) {
+    return '';
+  }
+
+  if (date === 'today' {
+    return date;
+  }
+
+  if (typeof date === 'string') {
+    date = new Date (date);
+  }
+
+  switch (type) {
+    case 'unix':
+      return Math.floor (date.getTime() / 1000);
+      break;
+
+    case 'long':
+      return date.toISOString();
+      break;
+
+    case 'short':
+      return date.toISOString().split ('T')[0];
+      break;
+  }
+}
+
+
+/**
  * Communicate with the API
  *
  * @param   {object}  params                     Method parameters
@@ -25,6 +63,9 @@ module.exports = async function ({
   key,
   origin = 'no-origin',
   timeout = 5000,
+  date = '',
+  start = '',
+  localtime = '',
 }) {
   delete arguments[0].key;
   delete arguments[0].origin;
@@ -40,9 +81,13 @@ module.exports = async function ({
     timeout,
     headers: {
       'Origin': origin,
-      'User-Agent': 'Node.js/worldtides',
+      'User-Agent': 'nodejs-worldtides',
     },
   };
+
+  options.parameters.date = fixDate (date, 'short');
+  options.parameters.start = fixDate (start, 'unix');
+  options.parameters.localtime = fixDate (localtime, 'long');
 
   const res = await doRequest (options);
   const data = JSON.parse (res.body);
